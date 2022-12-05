7767348b62d08b8c5367d7f30f3b34c0505957a1
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-05 09:33:32 -0500

    added buttons

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Calculator.css'
 
 export const Calculator = () => {
   return (
@@ -7,6 +8,25 @@ export const Calculator = () => {
         <div className='previous-operand'> </div>
         <div className='current-operand'> </div>
     </div>
+    <button className='span-two'>AC</button>
+    <button>DEL</button>
+    <button>÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>*</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button className='span-two'>=</button>
+
    </div>
   )
 }
