6a493287a5d7be29f21e96f8577135ee2607137d
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-05 09:21:13 -0500

    imported calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css'
+import { Calculator } from './components/Calculator';
 
 
 
 export default function App () {
     return(
-        <>
-        </>
+     < Calculator />
         );
 };
