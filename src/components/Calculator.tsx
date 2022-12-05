d61be139f932d0f88d186fb718abe706396592d6
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-05 09:21:53 -0500

    created a calculator component

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Calculator = () => {
+  return (
+    <div className='calculator-grid'>
+    <div className='output'>
+        <div className='previous-operand'> </div>
+        <div className='current-operand'> </div>
+    </div>
+   </div>
+  )
+}
